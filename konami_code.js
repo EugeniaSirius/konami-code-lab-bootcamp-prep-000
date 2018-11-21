034617bcdff79894810cab07c545f42806e4360c
Eugenia <eugenia.kaloshina@gmail.com>
2018-11-21 01:14:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,9 +12,9 @@ const codes = [
 ];
  
 function init() {
-      //const key=e.key
-   document.body.addEventListener('keydown', function(e) {
      
+   document.body.addEventListener('keydown', function(e) {
+      //const key=e.key
   let index = 0 
   let  key = parseInt(event.key)
      if (key===codes[index]) {
