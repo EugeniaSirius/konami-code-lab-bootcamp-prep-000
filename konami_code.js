1a7b6f2023c8e5c7cd00a7a92c0ee4545ea5270c
Eugenia <eugenia.kaloshina@gmail.com>
2018-11-21 01:19:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,8 +15,6 @@ function init() {
     let index = 0 
    document.body.addEventListener('keydown', function(e) {
     const key=e.key
-   
-  //let  key = parseInt(event.key)
      if (key===codes[index]) {
      index++
      if (index===codes.length)  { 
@@ -24,8 +22,7 @@ function init() {
        alert('Hurray!')
       } 
    } else {
-     //console.log(event.which)
-       index=0  
+     index=0  
    } 
  } )
-} //init()
+}
